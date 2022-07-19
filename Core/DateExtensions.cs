0692dd0a2c66b614168263981f9562b222f02225
Zoheir Karam <zkaram@wedgewood-inc.com>
2022-07-19 10:53:19 -0700

    first commit

---
+++ Core/DateExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System.Reflection;
+
+namespace Core;
+public static class DateExtensions
+{
+    public static T AppyTimeStrip<T>(this T sourceObject)
+    {
+        var dateProperties = sourceObject?.GetType().GetProperties().Where(m => m.PropertyType == typeof(DateTime?) || m.PropertyType == typeof(DateTime)).ToList();
+
+        dateProperties?.ForEach(prop =>
+        {
+
+            var stripTimeAttribute = prop.GetCustomAttribute(typeof(StripTime));
+
+            if (stripTimeAttribute != null)
+            {
+                var date = (prop.GetValue(sourceObject, null) as DateTime?);
+                prop.SetValue(sourceObject, (date == null ? null : date?.Date));
+            }
+        });
+
+        return sourceObject;
+    }
+
+    public static T ApplyDateTimeKind<T>(this T sourceObject) where T : class
+    {
+        var dateProperties = sourceObject.GetType().GetProperties().Where(m => m.PropertyType == typeof(DateTime?) || m.PropertyType == typeof(DateTime)).ToList();
+
+        dateProperties.ForEach(prop =>
+        {
+            var dateTimeKindAttribute = prop.GetCustomAttribute(typeof(SetKind));
+
+            if (dateTimeKindAttribute != null)
+            {
+                if (prop.PropertyType == typeof(DateTime?))
+                {
+                    var date = (DateTime?)prop.GetValue(sourceObject, null);
+                    if (date.HasValue)
+                    {
+                        var newDate = DateTime.SpecifyKind(date.Value, ((SetKind)dateTimeKindAttribute).Kind);
+                        prop.SetValue(sourceObject, newDate);
+                    }
+                }
+                else
+                {
+                    var date = prop.GetValue(sourceObject, null);
+                    if (date != null){
+                        var newDate = DateTime.SpecifyKind((DateTime)date, ((SetKind)dateTimeKindAttribute).Kind);
+                        prop.SetValue(sourceObject, newDate);
+                    }
+                }
+            }
+        });
+
+        return sourceObject;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class SetKind : Attribute
+{
+    public DateTimeKind Kind;
+    public SetKind(DateTimeKind kind)
+    {
+        this.Kind = kind;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class StripTime : Attribute
+{
+    public StripTime() { }
+}
---
+++ ExtensionsDemo/Demo.cs
@@ -0,0 +1,30 @@
+﻿using Core;
+
+var sample = new SampleObject()
+{
+    Date1 = new DateTime(2021, 10, 19, 19, 45, 33),
+    Date2 = new DateTime(2021, 10, 12, 12, 2, 39),
+    //Date3 = new DateTime(2023, 11, 13, 7, 34, 10),
+    Date4 = new DateTime(2023, 12, 14, 17, 12, 56)
+};
+
+sample.ApplyDateTimeKind().AppyTimeStrip();
+
+Console.WriteLine((sample.Date1.HasValue ? $"{sample.Date1} - Kind = {sample.Date1.Value.Kind}" : "null value"));
+Console.WriteLine((sample.Date2.HasValue ? $"{sample.Date2} - Kind = {sample.Date2.Value.Kind}" : "null value"));
+Console.WriteLine((sample.Date3.HasValue ? $"{sample.Date3} - Kind = {sample.Date3.Value.Kind}" : "null value"));
+Console.WriteLine((sample.Date4.HasValue ? $"{sample.Date4} - Kind = {sample.Date4.Value.Kind}" : "null value"));
+
+public class SampleObject
+{
+    public int Number1 { get; set; }
+    public string? SomeText { get; set; }
+    public DateTime? Date1 { get; set; }
+    [StripTime]
+    public DateTime? Date2 { get; set; }
+    [SetKind(DateTimeKind.Utc)]
+    [StripTime]
+    public DateTime? Date3 { get; set; }
+    [SetKind(DateTimeKind.Local)]
+    public DateTime? Date4 { get; set; }
+}
